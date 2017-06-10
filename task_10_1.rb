dd8ebcbdae18a73ca13c511891c76b65bcf37ff2
ChikaEJ <ecd4gg@gmail.com>
2017-06-10 12:23:19 +0600

    choose bog or small game

--- task_10_1.rb
+++ task_10_1.rb
@@ -1,35 +1,38 @@
-wapons = {
+weapons = {
   "stone" => ['lizzard', 'scissors'],
   "paper" => ['stone', 'spoke'],
   "scissors" => ['paper', 'lizzard'],
   "lizzard" => ['spoke', 'paper'],
   "spoke" => ['scissors', 'stone']
 }
-
-
-def cpu_chose(wapons)
-      cpu_chose = rand(1..5)
+small_game_weapons = {
+  "stone" => ['scissors'],
+  "paper" => ['stone'],
+  "scissors" => ['paper']
+}
+def cpu_chose(weapons)
+      cpu_chose = rand(1..weapons.length)
       if cpu_chose == 1
-        cpu_unit = wapons["stone"]
+        cpu_unit = weapons["stone"]
       elsif cpu_chose == 2
-        cpu_unit = wapons["paper"]
+        cpu_unit = weapons["paper"]
       elsif cpu_chose == 3
-        cpu_unit = wapons["scissors"]
+        cpu_unit = weapons["scissors"]
       elsif cpu_chose == 4
-        cpu_unit = wapons["lizzard"]
+        cpu_unit = weapons["lizzard"]
       else
-        cpu_unit = wapons["spoke"]
+        cpu_unit = weapons["spoke"]
       end
 end
-def choose_winer(wapons, user_unit, cpu_unit)
-  if user_unit.include?(wapons.key(cpu_unit))
-    p "You win!"
+def choose_winer(weapons, user_unit, cpu_unit)
+  if user_unit.include?(weapons.key(cpu_unit))
+    p "Cpu chose #{weapons.key(cpu_unit)}. You won with #{weapons.key(user_unit)}!"
     points = 1
   elsif user_unit == cpu_unit
-    p "Draw!"
+    p "Cpu chose #{weapons.key(cpu_unit)}. Draw! cause your chose also #{weapons.key(cpu_unit)}"
     points = "draw"
   else
-    p "You lost!"
+    p "Cpu chose #{weapons.key(cpu_unit)} and won your #{weapons.key(user_unit)}"
     points = 0
   end
   return points
@@ -38,39 +41,78 @@ rounds = 0
 draw = 0
 points = 0
 cpu_points = 0
-loop do
-p "Please choose wapon"
-user_chose = gets.chomp
-  case user_chose
-  when "stone" then user_unit = wapons["stone"]
-    cpu_unit = cpu_chose(wapons)
-    point = choose_winer(wapons, user_unit, cpu_unit)
-  when "spoke" then user_unit = wapons["spoke"]
-    cpu_unit = cpu_chose(wapons)
-    point = choose_winer(wapons, user_unit, cpu_unit)
-  when "paper" then user_unit = wapons["paper"]
-    cpu_unit = cpu_chose(wapons)
-    point = choose_winer(wapons, user_unit, cpu_unit)
-  when "lizzard" then user_unit = wapons["lizzard"]
-    cpu_unit = cpu_chose(wapons)
-    point = choose_winer(wapons, user_unit, cpu_unit)
-  when "scissors" then user_unit = wapons["scissors"]
-    cpu_unit = cpu_chose(wapons)
-    point = choose_winer(wapons, user_unit, cpu_unit)
-  when "exit"
-    p "Game over!"
-    break
-  else
-    p "Wring input!"
-    rounds = rounds - 1
+p "What game you want to play? (small, big)"
+game_version = gets.chomp
+case game_version
+when "small"
+  loop do
+  p "Please choose wapon (stone, paper, scissors)"
+  user_chose = gets.chomp
+    case user_chose
+    when "stone" then user_unit = weapons["stone"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "paper" then user_unit = weapons["paper"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "scissors" then user_unit = weapons["scissors"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "exit"
+      p "Game over!"
+      break
+    else
+      p "Wring input!"
+      rounds = rounds - 1
+    end
+
+    if point == "draw"
+      draw = draw + 1
+    elsif point == 1
+      points = points + 1
+    elsif point == 0
+      cpu_points = cpu_points + 1
+    end
+    rounds = rounds + 1
+    p "Rounds : #{rounds}, CPU: #{cpu_points}, Player: #{points}, Draw: #{draw}"
   end
-  if point == "draw"
-    draw = draw + 1
-  elsif point == 1
-    points = points + 1
-  elsif point == 0
-    cpu_points = cpu_points + 1
+when "big"
+  loop do
+  p "Please choose wapon (stone, paper, scissors, lizzard, spoke)"
+  user_chose = gets.chomp
+    case user_chose
+    when "stone" then user_unit = weapons["stone"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "paper" then user_unit = weapons["paper"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "scissors" then user_unit = weapons["scissors"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "spoke" then user_unit = weapons["spoke"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "lizzard" then user_unit = weapons["lizzard"]
+      cpu_unit = cpu_chose(weapons)
+      point = choose_winer(weapons, user_unit, cpu_unit)
+    when "exit"
+      p "Game over!"
+      break
+    else
+      p "Wring input!"
+      rounds = rounds - 1
+    end
+    if point == "draw"
+      draw = draw + 1
+    elsif point == 1
+      points = points + 1
+    elsif point == 0
+      cpu_points = cpu_points + 1
+    end
+    rounds = rounds + 1
+    p "Rounds : #{rounds}, CPU: #{cpu_points}, Player: #{points}, Draw: #{draw}"
   end
-  rounds = rounds + 1
-  p "Rounds : #{rounds}, CPU: #{cpu_points}, Player: #{points}, Draw: #{draw}"
+else
+  p "Wring input!"
 end

72d7bd40d4b003cf73f298e26802905815f4a779
ChikaEJ <ecd4gg@gmail.com>
2017-06-22 00:35:56 +0600

    Home work 13 class Car, UsedCar

--- task_13.rb
+++ task_13.rb
@@ -4,39 +4,70 @@ class Car
     @title = title
     @price = price
   end
-  @@cars = []
-
-  @@cars.push(Car.new(@title, @price))
-
-
+  def price_of_car
+    @price
+  end
 end
 
 class UsedCar < Car
-  attr_accessor :mileage
   def initialize(title, price, mileage)
-    @mileage = mileage
+    if mileage > 50000
+      @mileage = 50000
+    else
+      @mileage = mileage
+    end
     @price = price * (1 - mileage / 100000.0)
     @title = title
   end
-  @@cars.push(UsedCar.new(@title, @price, @mileage))
-  puts @@cars
+  def price_of_car
+    @price
+  end
 end
 
-class CarPriceCalculator < UsedCar
-  def initialize
-    @cars = @@cars
+class CarPriceCalculator
+  def initialize(cars)
+    @cars = cars
   end
-  def total_sum
+  def list_of_cars
+    total = 0
+    p sprintf("%-15s %15s", "Title", "Price")
     @cars.each do |car|
-      p car
-      sprintf("15% -7%", @title, @price)
+      p sprintf("%-15s %15.2f", car.title, car.price)
+      total += car.price
+    end
+    p sprintf("%-15s %15.2f", "Total sum:", total)
+  end
+end
+cars = []
+loop do
+  p "Do you want to add car to the list? Yes/No"
+  answer = gets.chomp.downcase
+  if answer != "yes" && answer != "no"
+    p "Please enter correct answer!"
+  else
+    case answer
+    when "yes"
+      p "Please enter car model"
+      car_model = gets.chomp.capitalize
+      p "Please enter price for this car"
+      price_of_car = gets.chomp.to_f
+      p "Please enter mileage for this car if it is used car"
+      mileage = gets.chomp.to_f
+      p mileage
+      if mileage == nil || mileage == 0.0
+        cars.push(Car.new(car_model, price_of_car))
+      else
+        cars.push(UsedCar.new(car_model, price_of_car, mileage))
+      end
+    when "no"
+      calc = CarPriceCalculator.new(cars)
+      calc.list_of_cars
+      break
     end
   end
 end
-audio = UsedCar.new("Auidio", 6000, 45000)
-audio_new = Car.new("Auidio", 6000)
-bmw = UsedCar.new("BMW", 8000, 32000)
-mersedess = Car.new("Mersedess Benz", 15000)
-p audio.title, audio.price, audio.mileage
-calc = CarPriceCalculator.new
-p calc.total_sum
+# audi = UsedCar.new("Audi", 6000)
+# audi_new = Car.new("Audi", 6000)
+# bmw = UsedCar.new("BMW", 8000, 32000)
+# mersedess = Car.new("Mersedess Benz", 15000)
+# cars = [audi, audi_new, bmw, mersedess]
